4c4db114e5bbf5f63153f0af69c4d649173422c9
Sreng Shuan <srengtrecom@yahoo.com>
2018-04-07 19:19:12 +0700

    initial commit

---
+++ app/Models/Crud.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Moloquent;
+use Illuminate\Database\Eloquent\Model;
+
+class Crud extends Moloquent
+{
+    protected $collection = 'crud';
+}
---
+++ app/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+
+class User extends Eloquent implements Authenticatable {
+    use Notifiable, AuthenticableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+//    protected $connection = 'mongodb';
+//    protected $collection = 'keywords';
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
